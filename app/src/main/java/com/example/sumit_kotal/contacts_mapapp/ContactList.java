df9d77def89c9d8e7a4733f7ec012a3b438e3504
sumit-kotal <kotalsumit@gmail.com>
2017-01-11 09:55:24 +0530

    Added Contacts Map Listview

--- app/src/main/java/com/example/sumit_kotal/contacts_mapapp/ContactList.java
+++ app/src/main/java/com/example/sumit_kotal/contacts_mapapp/ContactList.java
@@ -1,8 +1,8 @@
 package com.example.sumit_kotal.contacts_mapapp;
 
-import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.database.Cursor;
-import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
@@ -21,10 +21,10 @@ public class ContactList extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     View view;
+    ArrayList<String> contacts = new ArrayList<String>();
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     private ListView list;
 
     public ContactList() {
@@ -64,60 +64,120 @@ public class ContactList extends Fragment {
         // Inflate the layout for this fragment
         view =inflater.inflate(R.layout.fragment_contact_list, container, false);
         list = (ListView) view.findViewById(R.id.list);
-        new LoadContactsAyscn().execute();
+        readContacts();
         return view;
     }
 
-    class LoadContactsAyscn extends AsyncTask<Void, Void, ArrayList<String>> {
-        ProgressDialog pd;
 
-        @Override
-        protected void onPreExecute() {
-            // TODO Auto-generated method stub
-            super.onPreExecute();
+    public void readContacts() {
 
-            pd = ProgressDialog.show(getActivity(), "Loading Contacts",
-                    "Please Wait");
+        String name = null, phone = null, email = null, address = null;
+
+
+        ContentResolver cr = getActivity().getContentResolver();
+        Cursor cur = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR) {
+            cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                    null, null, null, null);
         }
 
-        @Override
-        protected ArrayList<String> doInBackground(Void... params) {
-            // TODO Auto-generated method stub
-            ArrayList<String> contacts = new ArrayList<String>();
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    System.out.println(name);
+                    //contacts.add(name);
 
-            Cursor c = getActivity().getContentResolver().query(
-                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                    null, null, null);
-            while (c.moveToNext()) {
+                    // get the phone number
+                    Cursor pCur = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR) {
+                        pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                    }
+                    while (pCur.moveToNext()) {
+                        phone = pCur.getString(
+                                pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        System.out.println(phone);
+                        //contacts.add(phone);
+                    }
+                    pCur.close();
 
-                String contactName = c
-                        .getString(c
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String phNumber = c
-                        .getString(c
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-                contacts.add(contactName + ":" + phNumber);
+                    // get email
 
+                    Cursor emailCur = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR) {
+                        emailCur = cr.query(
+                                ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                    }
+                    while (emailCur.moveToNext()) {
+                        // This would allow you get several email addresses
+                        // if the email addresses were stored in an array
+                        email = emailCur.getString(
+                                emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+
+                        System.out.println(email);
+                        // contacts.add(email);
+                    }
+                    emailCur.close();
+
+                    //Get Postal Address....
+
+                    String addrWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+                    String[] addrWhereParams = new String[]{id,
+                            ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE};
+                    Cursor addrCur = null;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+                        addrCur = cr.query(ContactsContract.Data.CONTENT_URI,
+                                null, addrWhere, addrWhereParams, null);
+                    }
+                    while (addrCur.moveToNext()) {
+                        String poBox = "";
+                        String street = "";
+                        String city = "";
+                        String state = "";
+                        String postalCode = "";
+                        String country = "";
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POBOX)) != null)
+                            poBox = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POBOX));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET)) != null)
+                            street = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY)) != null)
+                            city = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.REGION)) != null)
+                            state = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.REGION));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE)) != null)
+                            postalCode = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY)) != null)
+                            country = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY));
+
+                        address = poBox + street + city + state + postalCode + country;
+
+                        // Do something with these....
+                        System.out.println(address);
+                        //contacts.add(address);
+
+                    }
+                    addrCur.close();
+
+                    contacts.add(name + "\n" + phone + "\n" + email + "\n" + address);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            getActivity().getApplicationContext(), R.layout.text, contacts);
+
+                    list.setAdapter(adapter);
+                }
             }
-            c.close();
-
-            return contacts;
         }
-
-        @Override
-        protected void onPostExecute(ArrayList<String> contacts) {
-            // TODO Auto-generated method stub
-            super.onPostExecute(contacts);
-
-            pd.cancel();
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplication(), R.layout.text, contacts);
-
-            list.setAdapter(adapter);
-
-        }
-
     }
-
 }
--- app/src/main/java/com/example/sumit_kotal/contacts_mapapp/ContactMap.java
+++ app/src/main/java/com/example/sumit_kotal/contacts_mapapp/ContactMap.java
@@ -1,6 +1,10 @@
 package com.example.sumit_kotal.contacts_mapapp;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
@@ -98,5 +102,118 @@ public class ContactMap extends Fragment implements OnMapReadyCallback {
         googleMap.addMarker(new MarkerOptions().position(sydney)
                 .title("Marker in Sydney"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void readContacts() {
+
+        String name = null, phone = null, email = null, address = null;
+
+
+        ContentResolver cr = getActivity().getContentResolver();
+        Cursor cur = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR) {
+            cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                    null, null, null, null);
+        }
+
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    System.out.println(name);
+                    //contacts.add(name);
+
+                    // get the phone number
+                    Cursor pCur = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR) {
+                        pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                    }
+                    while (pCur.moveToNext()) {
+                        phone = pCur.getString(
+                                pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        System.out.println(phone);
+                        //contacts.add(phone);
+                    }
+                    pCur.close();
+
+
+                    // get email
+
+                    Cursor emailCur = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR) {
+                        emailCur = cr.query(
+                                ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                    }
+                    while (emailCur.moveToNext()) {
+                        // This would allow you get several email addresses
+                        // if the email addresses were stored in an array
+                        email = emailCur.getString(
+                                emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+
+                        System.out.println(email);
+                        // contacts.add(email);
+                    }
+                    emailCur.close();
+
+                    //Get Postal Address....
+
+                    String addrWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+                    String[] addrWhereParams = new String[]{id,
+                            ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE};
+                    Cursor addrCur = null;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+                        addrCur = cr.query(ContactsContract.Data.CONTENT_URI,
+                                null, addrWhere, addrWhereParams, null);
+                    }
+                    while (addrCur.moveToNext()) {
+                        String poBox = "";
+                        String street = "";
+                        String city = "";
+                        String state = "";
+                        String postalCode = "";
+                        String country = "";
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POBOX)) != null)
+                            poBox = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POBOX));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET)) != null)
+                            street = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY)) != null)
+                            city = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.REGION)) != null)
+                            state = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.REGION));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE)) != null)
+                            postalCode = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE));
+
+                        if (addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY)) != null)
+                            country = addrCur.getString(addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY));
+
+                        address = poBox + street + city + state + postalCode + country;
+
+                        // Do something with these....
+                        System.out.println(address);
+                        //contacts.add(address);
+
+                    }
+                    addrCur.close();
+/*
+                    contacts.add(name+"\n"+phone+"\n"+email+"\n"+address);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            getActivity().getApplicationContext(), R.layout.text, contacts);
+
+                    list.setAdapter(adapter);
+                    */
+                }
+            }
+        }
     }
 }
